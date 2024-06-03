3ea92e3f49b04e04da6f9ea7111eb5241209a49b
Savio GB <Saviogbeh@gmail.com>
2024-06-03 19:05:47 -0400

    lab 3

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double _counter = 0.0;
 
-
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
